0d9e7acb886f049821d1c102fb574ad7369be0cf
SimonFair <39065407+SimonFair@users.noreply.github.com>
2021-01-27 16:07:15 +0000

    Initial Beta Release

--- source/include/lib_usbip.php
+++ source/include/lib_usbip.php
@@ -1,6 +1,8 @@
 <?php
-/* Copyright 2015, Guilherme Jardim
- * Copyright 2016-2020, Dan Landon
+/*  Copyright 2021, Simon Fairweather
+ *  
+ * 
+ * based on original code from Guilherme Jardim and Dan Landon
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
@@ -20,66 +22,16 @@ $paths = [  "device_log"		=> "/tmp/{$plugin}/",
 			"ping_status"		=> "/var/state/{$plugin}/ping_status.json",
 			"hotplug_status"	=> "/var/state/{$plugin}/hotplug_status.json",
 			"remote_usbip"		=> "/tmp/{$plugin}/config/remote_usbip.cfg",
-
 		];
 
 $docroot = $docroot ?: @$_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
-$users = @parse_ini_file("$docroot/state/users.ini", true);
 $disks = @parse_ini_file("$docroot/state/disks.ini", true);
-
-if (! isset($var)){
-	if (! is_file("$docroot/state/var.ini")) shell_exec("/usr/bin/wget -qO /dev/null localhost:$(ss -napt | /bin/grep emhttp | /bin/grep -Po ':\K\d+') >/dev/null");
-	$var = @parse_ini_file("$docroot/state/var.ini");
-}
-
-if ((! isset($var['USE_NETBIOS']) || ((isset($var['USE_NETBIOS'])) && ($var['USE_NETBIOS'] == "yes")))) {
-	$use_netbios = "yes";
-} else {
-	$use_netbios = "no";
-}
-
-if ( is_file( "plugins/preclear.disk/assets/lib.php" ) )
-{
-	require_once( "plugins/preclear.disk/assets/lib.php" );
-	$Preclear = new Preclear;
-}
-else
-{
-	$Preclear = null;
-}
 
 #########################################################
 #############        MISC FUNCTIONS        ##############
 #########################################################
 
-class MiscUD
-{
 
-	public function save_json($file, $content)
-	{
-		file_put_contents($file, json_encode($content, JSON_PRETTY_PRINT ));
-	}
-
-	public function get_json($file)
-	{
-		return file_exists($file) ? @json_decode(file_get_contents($file), true) : [];
-	}
-
-	public function disk_device($disk)
-	{
-		return (file_exists($disk)) ? $disk : "/dev/{$disk}";
-	}
-
-	public function disk_name($disk)
-	{
-		return (file_exists($disk)) ? basename($disk) : $disk;
-	}
-
-	public function array_first_element($arr)
-	{
-		return (is_array($arr) && count($arr)) ? $arr[0] : $arr;
-	}
-}
 
 function is_ip($str) {
 	return filter_var($str, FILTER_VALIDATE_IP);
@@ -151,89 +103,6 @@ function exist_in_file($file, $val) {
 	return (preg_grep("%{$val}%", @file($file))) ? TRUE : FALSE;
 }
 
-function get_device_stats($mountpoint, $active=TRUE) {
-	global $paths, $plugin;
-
-	$tc = $paths['df_status'];
-	$df_status = is_file($tc) ? json_decode(file_get_contents($tc),TRUE) : array();
-	$rc = "";
-	if (is_mounted($mountpoint, TRUE)) {
-		if (isset($df_status[$mountpoint])) {
-			$rc = $df_status[$mountpoint]['stats'];
-		}
-		if (($active) && ((time() - $df_status[$mountpoint]['timestamp']) > 90) ) {
-			exec("/usr/local/emhttp/plugins/{$plugin}/scripts/get_ud_stats df_status {$tc} '{$mountpoint}' &");
-		}
-	}
-	return preg_split('/\s+/', $rc);
-}
-
-function get_disk_dev($dev) {
-	global $paths;
-
-	$rc		= basename($dev);
-	$sf		= $paths['dev_state'];
-	if (is_file($sf)) {
-		$devs = parse_ini_file($paths['dev_state'], true);
-		foreach ($devs as $d) {
-			if (($d['device'] == basename($dev)) && isset($d['name'])) {
-				$rc = $d['name'];
-				break;
-			}
-		}
-	}
-	return $rc;
-}
-
-function get_disk_reads_writes($dev) {
-	global $paths;
-
-	$dev	= (strpos($dev, "nvme") !== false) ? preg_replace("#\d+p#i", "", $dev) : preg_replace("#\d+#i", "", $dev) ;
-	$rc		= array();
-	$sf		= $paths['dev_state'];
-	if (is_file($sf)) {
-		$devs = parse_ini_file($paths['dev_state'], true);
-		foreach ($devs as $d) {
-			if (($d['device'] == basename($dev)) && isset($d['numReads']) && isset($d['numWrites'])) {
-				$rc[] = $d['numReads'];
-				$rc[] = $d['numWrites'];
-				break;
-			}
-		}
-	}
-	return $rc;
-}
-
-function is_disk_running($dev) {
-	global $paths;
-
-	$rc			= FALSE;
-	$run_devs	= FALSE;
-	$sf = $paths['dev_state'];
-	if (is_file($sf)) {
-		$devs = parse_ini_file($sf, true);
-		foreach ($devs as $d) {
-			if (($d['device'] == basename($dev)) && isset($d['spundown'])) {
-				$rc =($d['spundown'] == '0') ? TRUE : FALSE;
-				$run_devs = TRUE;
-				break;
-			}
-		}
-	}
-	if (! $run_devs) {
-		$tc = $paths['run_status'];
-		$run_status = is_file($tc) ? json_decode(file_get_contents($tc),TRUE) : array();
-		if (isset($run_status[$dev]) && (time() - $run_status[$dev]['timestamp']) < 60) {
-			$rc = ($run_status[$dev]['running'] == 'yes') ? TRUE : FALSE;
-		} else {
-			$state = trim(timed_exec(10, "/usr/sbin/hdparm -C $dev 2>/dev/null | /bin/grep -c standby"));
-			$rc = ($state == 0) ? TRUE : FALSE;
-			$run_status[$dev] = array('timestamp' => time(), 'running' => $rc ? 'yes' : 'no');
-			file_put_contents($tc, json_encode($run_status));
-		}
-	}
-	return $rc;
-}
 
 function is_usbip_server_online($ip, $mounted, $background=TRUE) {
 	global $paths, $plugin;
@@ -253,54 +122,6 @@ function is_usbip_server_online($ip, $mounted, $background=TRUE) {
 	return $is_alive;
 }
 
-
-
-function get_temp($dev, $running) {
-	global $var, $paths;
-
-	$rc	= "*";
-	if ($running) {
-		$temp = "";
-		$sf = $paths['dev_state'];
-		if (is_file($sf)) {
-			$devs = parse_ini_file($paths['dev_state'], true);
-			foreach ($devs as $d) {
-				if (($d['device'] == basename($dev)) && isset($d['temp'])) {
-					$temp = $d['temp'];
-					$rc = $temp;
-					break;
-				}
-			}
-		}
-		if ($temp == "") {
-			$tc = $paths['hdd_temp'];
-			$temps = is_file($tc) ? json_decode(file_get_contents($tc),TRUE) : array();
-			if (isset($temps[$dev]) && (time() - $temps[$dev]['timestamp']) < $var['poll_attributes'] ) {
-				$rc = $temps[$dev]['temp'];
-			} else {
-				$cmd	= "/usr/sbin/smartctl -n standby -A $dev | /bin/awk 'BEGIN{t=\"*\"} $1==\"Temperature:\"{t=$2;exit};$1==190||$1==194{t=$10;exit} END{print t}'";
-				$temp	= trim(timed_exec(10, $cmd));
-				$temp	= ($temp < 128) ? $temp : "*";
-				$temps[$dev] = array('timestamp' => time(), 'temp' => $temp);
-				file_put_contents($tc, json_encode($temps));
-				$rc = $temp;
-			}
-		}
-	}
-	return $rc;
-}
-
-function benchmark() {
-	$params   = func_get_args();
-	$function = $params[0];
-	array_shift($params);
-	$time     = -microtime(true); 
-	$out      = call_user_func_array($function, $params);
-	$time    += microtime(true); 
-	$type     = ($time > 10) ? "INFO" : "DEBUG";
-	usbip_log("benchmark: $function(".implode(",", $params).") took ".sprintf('%f', $time)."s.", $type);
-	return $out;
-}
 
 function timed_exec($timeout=10, $cmd) {
 	$time		= -microtime(true); 
@@ -375,104 +196,6 @@ function toggle_pass_through($sn, $status) {
 	return ($config[$sn]["pass_through"] == "yes") ? 'true' : 'false';
 }
 
-function execute_script($info, $action, $testing = FALSE) { 
-global $paths;
-
-	putenv("ACTION={$action}");
-	foreach ($info as $key => $value) {
-		putenv(strtoupper($key)."={$value}");
-	}
-	$cmd = $info['command'];
-	$bg = ($info['command_bg'] == "true" && $action == "ADD") ? "&" : "";
-	if ($common_cmd = get_config("Config", "common_cmd")) {
-		$common_script = $paths['scripts'].basename($common_cmd);
-		copy($common_cmd, $common_script);
-		@chmod($common_script, 0755);
-		usbip_log("Running common script: '".basename($common_script)."'");
-		exec($common_script, $out, $return);
-		if ($return) {
-			usbip_log("Error: common script failed with return '{$return}'");
-		}
-	}
-
-	if ($cmd) {
-		$command_script = $paths['scripts'].basename($cmd);
-		copy($cmd, $command_script);
-		@chmod($command_script, 0755);
-		usbip_log("Running device script: '".basename($cmd)."' with action '{$action}'.");
-
-		$script_running = is_script_running($cmd);
-		if ((! $script_running) || (($script_running) && ($action != "ADD"))) {
-			if (! $testing) {
-				if ($action == "REMOVE" || $action == "ERROR_UNMOUNT") {
-					sleep(1);
-				}
-				$cmd = isset($info['serial']) ? "$command_script > /tmp/{$info['serial']}.log 2>&1 $bg" : "$command_script > /tmp/".preg_replace('~[^\w]~i', '', $info['device']).".log 2>&1 $bg";
-
-				/* Set state as script running. */
-				$script_name = $info['command'];
-				$tc = $paths['script_run'];
-				$script_run = is_file($tc) ? json_decode(file_get_contents($tc),TRUE) : array();
-				$script_run[$script_name] = array('running' => 'yes','user' => 'no');
-				file_put_contents($tc, json_encode($script_run));
-
-				/* Run the script. */
-				exec($cmd, $out, $return);
-				if ($return) {
-					usbip_log("Error: device script failed with return '{$return}'");
-				}
-			} else {
-				return $command_script;
-			}
-		} else {
-			usbip_log("Device script '".basename($cmd)."' aleady running!");
-		}
-	}
-
-	return FALSE;
-}
-
-function remove_config_disk($sn) {
-
-	$config_file = $GLOBALS["paths"]["config_file"];
-	$config = @parse_ini_file($config_file, true);
-	if ( isset($config[$source]) ) {
-		usbip_log("Removing configuration '$source'.");
-	}
-	$command = $config[$source]['command'];
-	if ( isset($command) && is_file($command) ) {
-		@unlink($command);
-		usbip_log("Removing script '$command'.");
-	}
-	unset($config[$sn]);
-	save_ini_file($config_file, $config);
-	return (! isset($config[$sn])) ? TRUE : FALSE;
-}
-
-function is_disk_ssd($device) {
-
-	$rc		= FALSE;
-	$device	= (strpos($device, "nvme") !== false) ? preg_replace("#\d+p#i", "", $device) : preg_replace("#\d+#i", "", $device) ;
-	if (strpos($device, "nvme") === false) {
-		$file = "/sys/block/".basename($device)."/queue/rotational";
-		if (is_file($file)) {
-			$rc = (@file_get_contents($file) == 0) ? TRUE : FALSE;
-		} else {
-			usbip_log("Warning: Can't get rotational setting of '{$device}'.");
-		}
-	} else {
-		$rc = TRUE;
-	}
-	return $rc;
-}
-
-function spin_disk($down, $dev) {
-	if ($down) {
-		exec("/usr/local/sbin/emcmd cmdSpindown=$dev");
-	} else {
-		exec("/usr/local/sbin/emcmd cmdSpinup=$dev");
-	}
-}
 
 
 
@@ -529,7 +252,7 @@ function remove_config_remote_host($source) {
 /* Check modules loaded and commands exist. */
 
 function check_usbip_modules() {
-	global $loaded_usbip_host, $loaded_vhci_hcd, $usbip_cmds_exist ;
+	global $loaded_usbip_host, $loaded_vhci_hcd, $usbip_cmds_exist, $exists_vhci_hcd, $exists_usbip_host ;
 
 	exec("ls /usr/local/sbin/. | grep -c usbip*", $usbip_cmds_exist_array) ;
 	$usbip_cmds_exist = $usbip_cmds_exist_array[0] ;
@@ -540,10 +263,16 @@ function check_usbip_modules() {
 	exec("cat /proc/modules | grep -c usbip_host", $loaded_usbip_host_array) ;
 	$loaded_usbip_host = $loaded_usbip_host_array[0] ;
 
+	exec("find /lib/modules/ | grep -c usbip-host ", $exists_usbip_host_array) ;
+	$exists_usbip_host = $exists_usbip_host_array[0] ;
+
+	exec("find /lib/modules/ | grep -c vhci-hcd ", $exists_vhci_hcd_array) ;
+	$exists_vhci_hcd = $exists_vhci_hcd_array[0] ;
+
 }
 
 
-function get_unassigned_usb() {
+function get_usbip_devs() {
 	global $disks;
 
 	$ud_disks = $paths = $unraid_disks = $b =  array();
@@ -557,7 +286,7 @@ function get_unassigned_usb() {
 		$udevisplit=explode("=",$udevi) ;
 		$flash_udev[$udevisplit[0]] = $udevisplit[1] ;
 	}
-	#var_dump($flash_udev) ;
+	
 	
 	exec('usbip list -pl | sort'  ,$usbiplocal) ;
 	/* Build USB Device Array */
@@ -585,53 +314,28 @@ function get_unassigned_usb() {
 		else { 
 			$tj[$busid]["isflash"] = false ;
 		}
-		
-		#$tj[$busid]["udev"] = $fudev;
-      
 	}
-
-
 	return $tj ;
 }
 
 function get_all_usb_info($bus="all") {
 
-	usbip_log("Starting get_all_disks_info.", "DEBUG");
+	usbip_log("Starting get_all_usb_info.", "DEBUG");
 	$time = -microtime(true);
-	$ud_disks = get_unassigned_usb();
-	if (!is_array($ud_disks)) {
-		$ud_disks = array();
+	$usb_devs = get_usbip_devs();
+	if (!is_array($usb_devs)) {
+		$usb_devs = array();
 	}
 	usbip_log("Total time: ".($time + microtime(true))."s!", "DEBUG");
-/*	usort($ud_disks, create_function('$a, $b','$key="device";if ($a[$key] == $b[$key]) return 0; return ($a[$key] < $b[$key]) ? -1 : 1;')); */
-	return $ud_disks;
+
+	return $usb_devs;
 }
-
-function get_udev_info($device, $udev=NULL, $reload) {
-	global $paths;
-
-	$state = is_file($paths['state']) ? @parse_ini_file($paths['state'], true) : array();
-	if ($udev) {
-		$state[$device] = $udev;
-		save_ini_file($paths['state'], $state);
-		return $udev;
-	} else if (array_key_exists($device, $state) && (! $reload)) {
-		usbip_log("Using udev cache for '$device'.", "DEBUG");
-		return $state[$device];
-	} else {
-		$state[$device] = parse_ini_string(str_replace(array("$","!","\""), "", timed_exec(5,"/sbin/udevadm info --query=property --path $(/sbin/udevadm info -q path -n $device 2>/dev/null) 2>/dev/null")));
-		save_ini_file($paths['state'], $state);
-		usbip_log("Not using udev cache for '$device'.", "DEBUG");
-		return $state[$device];
-	}
-}
-
 
 
 function parse_usbip_port()
 {
 	exec('usbip port', $cmd_return) ;
-	#return $cmd_return ;
+
 	$port_number = 0 ;
     $ports=array() ;
 	foreach ($cmd_return as $line) {
@@ -696,8 +400,7 @@ function parse_usbip_remote($remote_host)
 		if 	   ($count>2) $remotes[$usbip_ip][$busid]["detail"][] = $line ;
 		$count=$count+1 ;
 	}
-	#asort($remotes) ;
-	
+		
 	return $remotes ;
 }
 ?>
